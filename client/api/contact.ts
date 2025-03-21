ce8d79341e6fb57215b2bb45e9ab33a0af27e279
Mohamed-Ali-Hammami <99651583+Mohamed-Ali-Hammami@users.noreply.github.com>
2025-03-22 00:19:16 +0100

    first commit

--- client/api/contact.ts
+++ client/api/contact.ts
@@ -1,10 +1,10 @@
 // api/contact.ts
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { storage } from '.././../../server/storage'; // adjust the path if needed
+import { storage } from '../../server/storage'; // adjust the path if needed
 import { insertContactSchema } from '@shared/schema';
 import { ZodError } from 'zod';
 import { fromZodError } from 'zod-validation-error';
-import { sendContactEmail } from '.././../../server/emailService'; // adjust the path as needed
+import { sendContactEmail } from '../../server/emailService'; // adjust the path as needed
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
